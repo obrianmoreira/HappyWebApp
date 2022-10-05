34f4188d2ae9952f53324c836f1ea49600803e07
obrianmoreira <brian.agnilo@gmail.com>
2022-10-05 18:55:42 +0200

    new version

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const Lead = mongoose.model('Lead', leadSchema);
     //res.render(__dirname + "/views/pages/index.ejs",{});
 //})
 
-/*leads.post('/', (req, res) =>{
+leads.post('/leads', (req, res) =>{
     console.log("chegou");
     let newLead = new Lead({
         name: req.body.name,
@@ -34,7 +34,7 @@ const Lead = mongoose.model('Lead', leadSchema);
     })
     newLead.save();
     res.redirect('https://happyclean-api.vercel.app/');
-})*/
+})
 
 app.listen(PORT, function(){
     console.log("Foi")
