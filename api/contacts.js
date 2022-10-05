b9cb05ab489e766a295d7dd2cf6871e259f185f8
obrianmoreira <brian.agnilo@gmail.com>
2022-10-05 22:54:19 +0200

    new version

--- api/contacts.js
+++ api/contacts.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const app = new express();
+const PORT = process.env.PORT || 3000;
 
-router.get('/', async (req, res) => {
-    try{
-        res.json({
-            status:200,
-            message: "Get data has done"
-        })
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("server error")
-    }
-});
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb+srv://obrianmoreira:KaplerB@cluster0.r5ewwpf.mongodb.net/happyDB?retryWrites=true&w=majority', {useNewUrlParser: true}, {useUnifiedTopology: true})
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/api/leads", leads);
+
+const leadSchema = {
+    name: String,
+    email: String,
+    phone: String
+}
+
+const Lead = mongoose.model('Lead', leadSchema);
+
+
+app.post('/api/', (req, res) =>{
+    console.log("chegou");
+    let newLead = new Lead({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    })
+    newLead.save();
+    res.redirect('https://happyclean-api.vercel.app/');
+})
+
+app.listen(PORT, function(){
+    console.log("Foi")
+})  
 
 module.exports = router;
